9a67bca662e2c08fe6c67bdd29c51d36814461aa
THAMOTHARA <MyPeopleApp@kannan.local>
2014-12-24 16:03:26 +0530

    fg
    fdg

---
+++ ipaddressfine.php
@@ -0,0 +1,6 @@
+<script type="application/javascript">
+function getip(json) {
+alert(json.ip);
+}
+</script>
+<script type="application/javascript" src="http://jsonip.appspot.com/?callback=getip"> </script>
